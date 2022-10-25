778c9c52e035af06b8820f2f93955efd63a669ca
Andrew Wang <34516018+wandwan@users.noreply.github.com>
2022-10-24 17:33:23 -0700

    Update TopologicalSort.java

--- src/Algorithms/TopologicalSort.java
+++ src/Algorithms/TopologicalSort.java
@@ -6,36 +6,33 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class TopologicalSort {
-	public static int[] TopoSort(ArrayList<Integer>[] adj) {
-		int[] ans = new int[adj.length];
-		int[] incoming = new int[adj.length];
-		int tot = 0;
-		for(int i = 0; i < adj.length; i++)
-			for(int j = 0; j < adj[i].size(); j++) {
-				incoming[adj[i].get(j)]++;
-				tot++;
-			}
-		ArrayDeque<Integer> q = new ArrayDeque<Integer>();
-		for(int i = 0; i < adj.length; i++) {
-			if(incoming[i] != 0)
-				continue;
-		q.add(i);
-		ans[i] = 0;
-			while(!q.isEmpty()) {
-				int curr = q.poll();
-				for(int j = 0; j < adj[curr].size(); j++) {
-					int n = adj[curr].get(j);
-					incoming[n]--;
-					tot--;
-					if(incoming[n] != 0)
-						continue;
-					ans[n] = ans[curr] + 1;
-					q.add(n);
-				}
-			}
-		}
-		if(tot <= 0)
-			return null;
-		return ans;
+	public static ArrayList<Integer> topoSort(ArrayList<Integer>[] adj) {
+		ArrayList<Integer> queue = new ArrayList<Integer>();
+        int[] outgoing = new int[adj.length];
+        for(int i = 0 ; i < adj.length; i++) {
+            outgoing[i] = adj[i].size();
+            if(outgoing[i] == 0)
+                queue.add(i);
+        }
+        adj = reverse(adj);
+        for(int i = 0; i < queue.size(); i++) {
+            int curr = queue.get(i);
+            for(int next: adj[curr])
+                if(--outgoing[next] == 0)
+                    queue.add(next);
+        }
+        if(queue.size() < adj.length)
+            return new ArrayList<Integer>();
+        Collections.reverse(queue);
+        return queue;
 	}
+    public static ArrayList<Integer>[] reverse(ArrayList<Integer>[] adj) {
+        ArrayList<Integer>[] reversed = new ArrayList[adj.length];
+        for(int i = 0; i < adj.length; i++)
+            reversed[i] = new ArrayList<Integer>();
+        for(int i = 0; i < adj.length; i++)
+            for(int end : adj[i])
+                reversed[end].add(i);
+        return reversed;
+    }
 }
